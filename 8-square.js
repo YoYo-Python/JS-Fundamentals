4ee4f80420474779b26dfb798cee5337f1017c7c
Youssef Waleed <yoyo.kotlin@gmail.com>
2025-07-13 00:41:44 +0300

    Add

--- 8-square.js
+++ 8-square.js
@@ -6,10 +6,6 @@ if (args[0] == undefined || containsLetter){
     console.log('Missing size')
 }
 
-
-
-
-
 for (let i = 0; i < args[0]; i++){
     console.log('X'.repeat(args[0]))
 }
---
+++ 9-add.js
@@ -0,0 +1,8 @@
+const args = process.argv.slice(2);
+
+
+function add(a,b){
+    console.log(a + b)
+}
+
+add(Number(args[0]), Number(args[1]));
