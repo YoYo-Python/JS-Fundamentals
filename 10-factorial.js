7228b98f42aedfa4533858db81be96db9d9bc976
Youssef Waleed <yoyo.kotlin@gmail.com>
2025-07-13 00:59:14 +0300

    Factorial

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,9 +1,13 @@
 const args = process.argv.slice(2);
 let value = parseInt(args[0]);
 
-let result = 1;
-for (let i = 2; i <= value; i++) {
-    result *= i;
+function factorial() {
+    let result = 1;
+    for (let i = 2; i <= value; i++) {
+        result *= i;
+    }
+
+    console.log(result);
 }
 
-console.log(result);
+factorial()
